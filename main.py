9cef0fcd135ccda4c5b5661e1c6b618fe78353c0
Gary Ho <yho@GarydeMacBook-Air.local>
2022-10-26 10:01:53 -0700

    Move env variable to env file

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Response
+from dotenv import load_dotenv
+import os
 
 app = FastAPI()
+load_dotenv()
 
-@app.get("/{apple}")
-async def root(apple):
+API = "https://graph.facebook.com/v15.0/me/messages?access_token="+os.getenv('PAGE_ACCESS_TOKEN')
 
-    return {"message": apple}
+@app.get("/api/webhook")
+def fbverify(request: Request):
+    if request.query_params.get("hub.mode") == "subscribe" and request.query_params.get(
+        "hub.challenge"
+    ):
+        if (
+            not request.query_params.get("hub.verify_token")
+            == os.getenv('VERIFY_TOKEN')
+        ):
+            return Response(content="Verification token mismatch", status_code=403)
+        return Response(content=request.query_params["hub.challenge"])
+
+    return Response(content="Required arguments haven't passed.", status_code=400)
