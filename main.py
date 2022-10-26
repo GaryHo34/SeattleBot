24c3bf138474fcda47374e95c2d1e570613a952a
Gary Ho <yho@GarydeMacBook-Air.local>
2022-10-26 09:50:36 -0700

    test change

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Response
+import os
 
 app = FastAPI()
 
-@app.get("/{apple}")
-async def root(apple):
+PAGE_ACCESS_TOKEN = "EAAG4nCYakrABAP0b8wZCdSxftpoWZCsPVaRu6KtguEBvtILnosYHYGJjoiMHrELZBsClHE0sVdWUpedM1Rto6tTHk2qZCEvTMlfVINqm0MWGCbOZBM28Q2dXZCeahDa8bFXGrnI6dDc8QRpqTKZCpEHLjh9kwctKVjrQFx5EZCRbHy8nklO6MfPZATUqixZBaAnMkZC2ZAZAKjVmOegZDZD"
+api = "https://graph.facebook.com/v15.0/me/messages?access_token="+PAGE_ACCESS_TOKEN
 
-    return {"message": apple}
+@app.get("/")
+def fbverify(request: Request):
+    if request.query_params.get("hub.mode") == "subscribe" and request.query_params.get(
+        "hub.challenge"
+    ):
+        if (
+            not request.query_params.get("hub.verify_token")
+            == "YOUR_VERIFY_TOKEN"
+        ):
+            return Response(content="Verification token mismatch", status_code=403)
+        return Response(content=request.query_params["hub.challenge"])
+
+    return Response(content="Required arguments haven't passed.", status_code=400)
+
