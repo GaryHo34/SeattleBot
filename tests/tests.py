dce3517bd48922d716d3f73891677f02348a007f
Gary Ho <garyho0916@gmail.com>
2022-12-30 21:04:43 -0800

    add test case and update readme

---
+++ tests/tests.py
@@ -0,0 +1,22 @@
+import unittest
+from model import *
+
+
+class TestCase(unittest.TestCase):
+
+    # https://developers.facebook.com/docs/messenger-platform/send-messages/buttons
+    def test_postback_button(self):
+        expected_button = {
+            "type": "postback",
+            "title": "<BUTTON_TEXT>",
+            "payload": "<STRING_SENT_TO_WEBHOOK>"
+        }
+        postback_button = PostbackButton(
+            title="<BUTTON_TEXT>",
+            payload="<STRING_SENT_TO_WEBHOOK>"
+        )
+        self.assertEqual(expected_button, postback_button)
+
+
+if __name__ == '__main__':
+    unittest.main()
