8fc086c3f16a9b2fee50cf2d0a95be0ba607eaac
Gary Ho <garyho0916@gmail.com>
2022-12-30 20:10:34 -0800

    added depends feature in fastapi  verify signature

--- utils.py
+++ utils.py
@@ -1,6 +1,9 @@
 import requests
-from model import Event
+from fastapi import Header, Request, Response
+from config import META_APP_SECRET
+from model import Event, WebhookRequestData
 from typing import Optional, List
+import hmac
 
 
 def get(
@@ -51,16 +54,34 @@ def post(
     return None
 
 
-def event_parser(event):
+async def verify_payload(request: Request, X_Hub_Signature_256: str = Header()):
+    data = await request.body()
+    expectedHash = hmac.new(
+        META_APP_SECRET.encode(), data, "sha256").hexdigest()
+    if expectedHash != X_Hub_Signature_256[7:]:
+        return Response(content="Couldn't validate the request signature.", status_code=401)
+
+
+def get_event(event):
     postback = event.get("postback")
     message = event.get("message")
-    quick_rp =  message.get('quick_reply') if message else None
+    quick_rp = message.get('quick_reply') if message else None
     sender_id = event["sender"]["id"]
 
     return Event(
         type='message' if message else 'postback' if postback else '',
         sender=sender_id,
         text=message.get('text') if message else '',
-        quick_reply= quick_rp.get('payload') if quick_rp else '',
+        quick_reply=quick_rp.get('payload') if quick_rp else '',
         payload=postback.get('payload') if postback else '',
     )
+
+
+def event_parser(data: WebhookRequestData) -> List[Event]:
+    if data.object != "page" or not data.entry:
+        return []
+
+    if not data.entry[0].get("messaging"):
+        return []
+
+    return list(map(get_event, data.entry[0].get("messaging")))
